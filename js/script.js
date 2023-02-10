152903856ba4946e99a5c113057ed17732367d1c
ketevaniveshaguri <ketevaniveshaguri@gmail.com>
2023-02-11 02:17:47 +0400

    personal-info-update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+function generateResume() {
+  let name = document.getElementById("name").value;
+  let surName = document.getElementById("surname").value;
+  let aboutMe = document.getElementById("form-about-me").value;
+  let about = document.getElementById("about-me").value;
+  let number = document.getElementById("input-number").value;
+  let mail = document.getElementById("input-mail").value;
 
+  const resumeHTML = `
+  <h1>${name + " " + surName}</h1> 
+  <p class="el-post">${mail}</p>
+  <p class="mob-number">${number}</p>
+
+  <p class="about">${aboutMe}</p> 
+  
+
+
+
+
+
+`;
+  resume.innerHTML = resumeHTML;
+}
